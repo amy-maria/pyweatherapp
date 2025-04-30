8fdf87f2093c117890ba2e5a22c0250762d172dd
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-04-30 16:22:21 -0400

    adding Django templates

--- weatherapp/posts/models.py
+++ weatherapp/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+# Django will authomatically add an s to the class name in dashboard
+class Post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField(unique=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
