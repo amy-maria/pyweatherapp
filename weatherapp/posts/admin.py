4046404cbac5ee907f3e7a5150061cfedf0286f4
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-04-30 16:22:21 -0400

    adding Django templates

--- weatherapp/posts/admin.py
+++ weatherapp/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date")  # Customize fields to show in the list
+    prepopulated_fields = {"slug": ("title",)}  # Auto-fill slug from title
