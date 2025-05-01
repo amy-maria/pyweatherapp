47d98f5fb4a6fa418916939c170a62eda95176ae
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-05-01 15:51:02 -0400

    added login logic and forms

--- weatherapp/posts/urls.py
+++ weatherapp/posts/urls.py
@@ -5,5 +5,6 @@ app_name = "posts"  # designates urlpatters are inside the posts app
 
 urlpatterns = [
     path("", views.posts_list, name="list"),
+    path("new-post/", views.post_new, name="new-post"),  # required above slug
     path("<slug:slug>/", views.post_page, name="page"),
 ]
--- weatherapp/users/urls.py
+++ weatherapp/users/urls.py
@@ -5,4 +5,6 @@ app_name = "users"  # designates urlpatters are inside the posts app
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
