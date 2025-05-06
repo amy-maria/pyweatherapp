99f755d2775a6e21a9401ef7d61594c6b0b2da1c
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-05-06 18:03:27 -0400

    fixed static file

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
-STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR / "weatherapp" / "staticfiles")
 # MEDIA_ROOT = BASE_DIR / "media"
 STATICFILES_DIRS = [
     BASE_DIR / "weatherapp" / "static",
