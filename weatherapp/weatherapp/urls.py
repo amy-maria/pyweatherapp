6bf10489625a62078a543adeee4af14df4c10ec3
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-05-07 16:54:55 -0400

    import settings fix

--- weatherapp/weatherapp/urls.py
+++ weatherapp/weatherapp/urls.py
@@ -19,13 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from weatherapp.weather.views import home
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # from weatherapp.weather.views import home
 
 # import your view
 
 # from django.views.static import serve
-from django.conf.urls.static import static
 
 
 urlpatterns = [
