de1d150d263756a7b923a8e96f8c999e78fb6a79
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-05-07 11:00:04 -0400

    fixed installed app/posts

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "weatherapp.posts",
+    "posts",
     "weatherapp.users",
     "weatherapp.weather",
 ]
