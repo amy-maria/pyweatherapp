e53fb0e929967ad21eec11105a4d8049d4166457
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-05-07 20:39:30 -0400

    push final changes

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -84,17 +84,16 @@ WSGI_APPLICATION = "weatherapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#  "default": {
+#     "ENGINE": "django.db.backends.#sqlite3",
+#    "NAME": BASE_DIR / "db.sqlite3",
+# }
+# }
 import dj_database_url
 import os
 
-DATABASES["default"] = dj_database_url.config(default="sqlite:///db.sqlite3")
-
+DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
