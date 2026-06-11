7b62b22ec32afdf4ff88471450436e2ef675a87d
Amy Rowell <amymaria75@gmail.com>
2026-06-11 18:38:41 -0400

    Update app include paths for root execution compatibility

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -19,7 +19,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, os.path.join(BASE_DIR, 'weatherapp'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "posts",
-    "users",
-    "weather",
+    "weatherapp.posts",
+    "weatherapp.users",
+    "weatherapp.weather", ,
 ]
 
 MIDDLEWARE = [
