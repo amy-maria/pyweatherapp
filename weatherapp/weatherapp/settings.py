8a05afecb1b00eb47da1c76ea5917de23770a91b
Amy Rowell <amymaria75@gmail.com>
2026-06-10 21:50:19 -0400

    fixed typo

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -14,7 +14,7 @@ import dj_database_url
 from pathlib import Path
 from decouple import config
 import os
-import dj_dataase_url
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
