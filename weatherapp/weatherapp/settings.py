505a66f529f446051be5dca04a54d04426750f5c
Amy Rowell <amymaria75@gmail.com>
2026-06-11 18:28:36 -0400

    Add missing sys import to settings

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -14,6 +14,7 @@ import dj_database_url
 from pathlib import Path
 from decouple import config
 import os
+import sys
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
